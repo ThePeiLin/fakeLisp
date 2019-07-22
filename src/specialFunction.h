9c8ca27326c67a3aa1baec1a3f88d25e5dd2a7bf
ThePeiLin <47449134+ThePeiLin@users.noreply.github.com>
2019-07-22 17:49:51 +0800

    Add files via upload

---
+++ src/specialFunction.h
@@ -0,0 +1,9 @@
+#ifndef _SPECIAL_FUNCTION_H_
+#define _SPECIAL_FUNCTION_H_
+#include"fake.h"
+void T_add(listTreeNode*);
+void T_sub(listTreeNode*);
+void T_mul(listTreeNode*);
+void T_div(listTreeNode*);
+void T_exit(listTreeNode*);
+#endif

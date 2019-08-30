aecec70d96ac8cb5a014b38073dcb4f22b0cdc27
ThePeiLin <47449134+ThePeiLin@users.noreply.github.com>
2019-08-30 22:32:31 +0800

    Add files via upload

---
+++ numAndString.h
@@ -0,0 +1,7 @@
+#ifndef _FLOAT_AND_STRING_H_
+#define _FLOAT_AND_STRING_H_
+char* floatToString(double);
+double stringToFloat(const char*);
+char* intToString(long);
+long stringToInt(const char*);
+#endif

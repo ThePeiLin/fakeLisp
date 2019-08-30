070dc13fd7131ec0ec57b7e5910fd8e67ed48b91
ThePeiLin <47449134+ThePeiLin@users.noreply.github.com>
2019-08-30 22:07:08 +0800

    Add files via upload

--- src/functionAndForm.h
+++ src/functionAndForm.h
@@ -5,6 +5,7 @@ void T_add(listTreeNode*);
 void T_sub(listTreeNode*);
 void T_mul(listTreeNode*);
 void T_div(listTreeNode*);
+void T_mod(listTreeNode*);
 void T_exit(listTreeNode*);
 void T_car(listTreeNode*);
 void T_cdr(listTreeNode*);

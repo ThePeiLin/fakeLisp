a98854f77475957f1d9e776bd9d7c93c14566f94
ThePeiLin <47449134+ThePeiLin@users.noreply.github.com>
2019-11-16 17:08:40 +0800

    Add files via upload

--- src/fake.h
+++ src/fake.h
@@ -2,21 +2,21 @@
 #define TREE_H_
 #include<stddef.h>
 #include"fakedef.h"
-#define OUTOFMEMORY 1
-defines* findDefine(const char);
-void errors(int);
+defines* findDefine(const char,env*);
 char* getListFromFile(FILE*);
-char* getStringFromList(const char* str);
-listTreeNode* becomeTree(const char*);
-listTreeNode* eval(listTreeNode*);
-int addFunction(char*,void(*)(listTreeNode*));
-int addDefine(const char*,void*,enum TYPES);
-void callFunction(listTreeNode*);
-void (*(findFunction(const char*)))(listTreeNode*);
-void returnTree(listTreeNode*);
-void printList(listTreeNode*,FILE*);
+char* getStringFromList(const char*);
+branch* becomeTree(const char*);
+branch* eval(branch*,env*);
+int addFunction(char*,void(*)(listTreeNode*,env*));
+defines* addDefine(const char*,const branch*,env*);
+void callFunction(listTreeNode*,env*);
+void (*(findFunction(const char*)))(listTreeNode*,env*);
+void returnTree(branch*);
+void printList(branch*,FILE*);
 listTreeNode* createNode(listTreeNode* const);
-listTreeNode* copyTree(listTreeNode*);
-listTreeNode* deleteTree(listTreeNode*);
-listTreeNode* deleteNode(listTreeNode*);
+branch* createBranch();
+branch* copyTree(branch*);
+branch* deleteTree(branch*);
+branch* deleteNode(branch*);
+env* newEnv();
 #endif

5f9e66a119ce36723d169db3e3550f031b98e32f
ThePei <pei_hang@qq.com>
2020-01-25 13:19:58 +0800

    new file

---
+++ src/tool.h
@@ -0,0 +1,19 @@
+#ifndef TOOL_H
+#define TOOL_H
+#include"fakedef.h"
+#define OUTOFMEMORY 1
+typedef struct RawString
+{
+	int len;
+	char * str;
+} rawString;
+int power(int,int);
+rawString getStringBetweenMarks(const char*);
+void printRawString(const char*,FILE*);
+char* floatToString(double);
+double stringToFloat(const char*);
+char* intToString(long);
+long stringToInt(const char*);
+int concmp(cell*,cell*);
+void errors(int);
+#endif

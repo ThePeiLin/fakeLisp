dacff5d89d53fda0083fe2f4759c13cb409fb753
ThePei <pei_hang@qq.com>
2020-02-23 11:54:29 +0800

    new function

--- src/form.c
+++ src/form.c
@@ -15,11 +15,11 @@ cptr** dealArg(cptr* argCptr,int num)
 		else
 		{
 			args[i]=createCptr(NULL);
-			args[i]->type=((cell*)argCptr->value)->left.type;
-			args[i]->value=((cell*)argCptr->value)->left.value;
-			((cell*)argCptr->value)->left.type=nil;
-			((cell*)argCptr->value)->left.value=NULL;
-			argCptr=&((cell*)argCptr->value)->right;
+			args[i]->type=((cell*)argCptr->value)->car.type;
+			args[i]->value=((cell*)argCptr->value)->car.value;
+			((cell*)argCptr->value)->car.type=nil;
+			((cell*)argCptr->value)->car.value=NULL;
+			argCptr=&((cell*)argCptr->value)->cdr;
 		}
 	}
 	if(tmpCptr!=NULL)
@@ -48,7 +48,7 @@ int coutArg(cptr* argCptr)
 	while(argCptr->type==cel)
 	{
 		num++;
-		argCptr=&((cell*)argCptr->value)->right;
+		argCptr=&((cell*)argCptr->value)->cdr;
 	}
 	return num;
 }
@@ -63,7 +63,7 @@ void deleteArg(cptr** args,int num)
 int N_quote(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,1);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,1);
 	replace(objCptr,args[0]);
 	deleteArg(args,1);	
 	return 0;
@@ -72,10 +72,10 @@ int N_quote(cptr* objCptr,env* curEnv)
 int N_car(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,1);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,1);
 	eval(args[0],curEnv);
 	if(args[0]->type==atm||args[0]->type==nil)return SYNTAXERROR;
-	replace(objCptr,&((cell*)args[0]->value)->left);
+	replace(objCptr,&((cell*)args[0]->value)->car);
 	deleteArg(args,1);
 	return 0;
 }
@@ -83,10 +83,10 @@ int N_car(cptr* objCptr,env* curEnv)
 int N_cdr(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,1);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,1);
 	eval(args[0],curEnv);
 	if(args[0]->type==atm||args[0]->type==nil)return SYNTAXERROR;
-	replace(objCptr,&((cell*)args[0]->value)->right);
+	replace(objCptr,&((cell*)args[0]->value)->cdr);
 	deleteArg(args,1);
 	return 0;
 }
@@ -94,13 +94,13 @@ int N_cdr(cptr* objCptr,env* curEnv)
 int N_cons(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,2);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,2);
 	eval(args[0],curEnv);
 	eval(args[0],curEnv);
 	objCptr->type=cel;
 	cell* tmpCell=objCptr->value=createCell((cell*)objCptr->outer);
-	replace(&tmpCell->left,args[0]);
-	replace(&tmpCell->right,args[1]);
+	replace(&tmpCell->car,args[0]);
+	replace(&tmpCell->cdr,args[1]);
 	deleteArg(args,2);
 	return 0;
 }
@@ -108,10 +108,10 @@ int N_cons(cptr* objCptr,env* curEnv)
 int N_eq(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,2);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,2);
 	eval(args[0],curEnv);
 	eval(args[1],curEnv);
-	if(cel(args[0],args[1])==0)
+	if(cptrcmp(args[0],args[1])==0)
 	{
 		objCptr->type=nil;
 		objCptr->value=NULL;
@@ -128,7 +128,7 @@ int N_eq(cptr* objCptr,env* curEnv)
 int N_atom(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,1);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,1);
 	eval(args[0],curEnv);
 	if(args[0]->type!=cel)
 	{
@@ -138,7 +138,7 @@ int N_atom(cptr* objCptr,env* curEnv)
 	else
 	{
 		cell* tmpCell=objCptr->value;
-		if(tmpCell->left.type==nil&&tmpCell->right.type==nil)
+		if(tmpCell->car.type==nil&&tmpCell->cdr.type==nil)
 		{
 			objCptr->type=atm;
 			objCptr->value=createAtom(num,"1",objCptr->outer);
@@ -156,7 +156,7 @@ int N_atom(cptr* objCptr,env* curEnv)
 int N_null(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,1);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,1);
 	eval(args[0],curEnv);
 	if(args[0]->type==nil)
 	{
@@ -166,7 +166,7 @@ int N_null(cptr* objCptr,env* curEnv)
 	else if(args[0]->type==cel)
 	{
 		cell* tmpCell=args[0]->value;
-		if(tmpCell->left.type==nil&&tmpCell->right.type==nil)
+		if(tmpCell->car.type==nil&&tmpCell->cdr.type==nil)
 		{
 			objCptr->type=atm;
 			objCptr->value=createAtom(num,"1",objCptr->outer);
@@ -189,20 +189,48 @@ int N_null(cptr* objCptr,env* curEnv)
 int N_cond(cptr* objCptr,env* curEnv)
 {
 	deleteCell(objCptr);
-	int argn=coutArg(&((cell*)objCptr->outer)->right);
-	cptr** args=dealArg(&((cell*)objCptr->outer)->right,aegn);
+	int argn=coutArg(&((cell*)objCptr->outer)->cdr);
+	cptr** args=dealArg(&((cell*)objCptr->outer)->cdr,argn);
 	int i;
-	for(i=0;i<num;i++)
+	for(i=0;i<argn;i++)
 	{
-		if(args[i]->type!=cel)return SYNTAXERROR;
-		cptr* condition=&((cell*)args[i]->value)->left;
+		if(args[i]->type!=cel)
+		{
+			deleteArg(args,argn);
+			return SYNTAXERROR;
+		}
+		cptr* condition=&((cell*)args[i]->value)->car;
 		eval(condition,curEnv);
-		if(condtition->type==atm)break;
+		if(condition->type==atm)break;
 		else if(condition->type==cel)
 		{
 			cell* tmpCell=(cell*)condition->value;
-			if(tmpCell->left.type!=nil||tmpCell->right.type!=nil)break;
+			if(tmpCell->car.type!=nil||tmpCell->cdr.type!=nil)break;
+			else
+			{
+				if(i=argn-1)
+				{
+					objCptr->type=nil;
+					objCptr->value=NULL;
+					deleteArg(args,argn);
+					return 0;
+				}
+			}
+		}
+		else if(condition->type==nil&&i==argn-1)
+		{
+			objCptr->type=nil;
+			objCptr->value=NULL;
+			deleteArg(args,argn);
+			return 0;
 		}
 	}
-	deleteArg(args,num);
+	int j;
+	int resNum=coutArg(&((cell*)args[i]->value)->cdr);
+	cptr** result=dealArg(&((cell*)args[i]->value)->cdr,resNum);
+	for(j=0;j<resNum;j++)eval(result[j],curEnv);
+	replace(objCptr,result[resNum-1]);
+	deleteArg(result,resNum);
+	deleteArg(args,argn);
+	return 0;
 }

0a3f46cec2c59b4419e91e6bf7a7f770a093e3d7
ThePei <pei_hang@qq.com>
2020-03-15 20:35:30 +0800

    add new function.

--- src/prepare.c
+++ src/prepare.c
@@ -1,23 +1,89 @@
-static macro* Head=NULL
-int macroMatch(cptr*)
+#include"prepare.h"
+#include"fake.h"
+static macro* Head=NULL;
+static masym* First=NULL;
+int macroMatch(cptr* objCptr)
 {
+	
 }
 
-macro* createMacro(cptr* format,cptr* express)
+int addMacro(cptr* format,cptr* express)
 {
-	macro* tmp=NULL;
-	if(!(tmp=(macro*)malloc(sizeof(macro))))
-		errors(OUTOFMEMORY);
-	tmp->format=format;
-	tmp->express=express;
-	tmp->next=NULL;
-	return tmp;
+	if(format->type!=cel)return SYNTAXERROR;
+	macro* current=Head;
+	while(current!=NULL&&!cptrcmp(format,current->format))
+		current=current->next;
+	if(current==NULL)
+	{
+		if(!(current=(macro*)malloc(sizeof(macro))))errors(OUTOFMEMORY);
+		current->format=format;
+		current->express=express;
+		current->next=Head;
+		Head=current;
+	}
+	else
+		current->express=express;
+	return 0;
 }
 
-int addMacro(macro* obj)
+masym* findMasym(const char* name)
 {
-	macro* current=(Head==NULL)?NULL:Head;
-	while(current->next!=NULL)current=current->next;
-	current->next=obj;
-	Head=(Head==NULL)?current:Head;
+	masym* current=First;
+	while(current!=NULL&&!strcmp(current->symName,name))current=current->next;
+	return current;
+}
+
+int fmatcmp(cptr* origin,cptr* format)
+{
+	cell* tmpCell=(format->type==cel)?format->value:NULL;
+	cell* forCell=tmpCell;
+	cell* oriCell=(origin->type==cell)?origin:NULL;
+	while(origin!=NULL)
+	{
+		if(format->type==cel&&origin->type==cel)
+		{
+			forCell=format->value;
+			oriCell=origin->value;
+			format=&forCell->car;
+			origin=&oriCell->car;
+			continue;
+		}
+		else if(format->type==atm)
+		{
+			atom* tmpAtm=format->value;
+			masym* tmpSym=findMasym(tmpAtm->value);
+			if(tmpSym==NULL)
+				if(!cptrcmp(origin,format))return 0;
+			if(!tmpSym->Func(origin))return 0;
+			if(oriCell!=NULL&&origin==&oriCell->car)
+			{
+				origin=&oriCell->cdr;
+				format=&forCell->cdr;
+				continue;
+			}
+		}
+		else if(oriCell!=NULL&&origin==&origin->cdr)
+		{
+			cell* oriPrev=NULL;
+			cell* forPrev=NULL;
+			if(oriCell->prev==NULL)break;
+			while(oriCell->prev!=NULL)
+			{
+				oriPrev=oriCell;
+				forPrev=forCell;
+				oriCell=oriCell->prev;
+				forCell=forCell->prev;
+				if(oriPrev==oriCell->car.value)break;
+			}
+			if(oriCell!=NULL)
+			{
+				origin=&oriCell->cdr;
+				format=&forCell->cdr;
+				continue;
+			}
+			if(forCell==tmpCell&&oriCell=&oriCell-cdr)break;
+		}
+		if(oriCell==NULL&&forCell==NULL)break;
+	}
+	return 1;
 }

975dfbe6606eb0d0a1ea18f4001896455d974be1
ThePei <pei_hang@qq.com>
2020-08-07 10:30:03 +0800

    fix bugs

--- src/fakeLisp.h
+++ src/fakeLisp.h
@@ -1,7 +1,7 @@
 #ifndef FAKELISP_H
 #define FAKELISP_H
 #include"fakedef.h"
-cptr* evalution(const char*,int,intpr*);
+cptr* evalution(const char*,intpr*);
 void initEvalution();
 int isscript(const char*);
 void runIntpr(intpr*);

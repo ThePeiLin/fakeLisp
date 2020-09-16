f0048ae606c7d584f4f7c297b319ba62d9b16984
ThePei <pei_hang@qq.com>
2020-09-16 23:05:18 +0800

    fix bugs and add new byte code

--- src/fakeLisp.c
+++ src/fakeLisp.c
@@ -9,6 +9,581 @@
 #include"syntax.h"
 #include"compiler.h"
 #include"fakeVM.h"
+#include"opcode.h"
+
+char* builtInSymbolList[]=
+{
+	"else",
+	"nil",
+	"cons",
+	"car",
+	"cdr",
+	"atom",
+	"null",
+	"ischr",
+	"isint",
+	"isdbl",
+	"isstr",
+	"issym",
+	"isprc",
+	"eq",
+	"gt",
+	"ge",
+	"lt",
+	"le",
+	"not",
+	"dbl",
+	"str",
+	"sym",
+	"chr",
+	"int",
+	"add",
+	"sub",
+	"mul",
+	"div",
+	"mod",
+	"gchstr",
+	"strlen",
+	"strcat",
+	"open",
+	"close",
+	"getc",
+	"ungetc",
+	"write",
+	"tell",
+	"seek",
+	"rewind",
+};
+
+byteCode P_cons=
+{
+	25,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_PAIR,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_POP_CAR,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_POP_CDR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_car=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_PUSH_CAR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_cdr=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_PUSH_CDR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_atom=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_ATOM,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_null=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_NULL,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_isint=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_INT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_ischr=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_CHR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_isdbl=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_DBL,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_isstr=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_STR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_issym=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_SYM,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_isprc=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_IS_PRC,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_eq=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_EQ,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_gt=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_GT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_ge=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_GE,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_lt=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_LT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_le=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_LE,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_not=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_NOT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_dbl=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CAST_TO_DBL,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_str=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CAST_TO_STR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_sym=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CAST_TO_SYM,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_chr=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CAST_TO_CHR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_int=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CAST_TO_INT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_add=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_ADD,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_sub=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_SUB,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_mul=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_MUL,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_div=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_DIV,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_mod=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_MOD,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_gchstr=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_GET_CHR_STR,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_strlen=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_STR_LEN,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_strcat=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_STR_CAT,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_open=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_OPEN,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_close=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_CLOSE,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_getc=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_GETC,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_ungetc=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_UNGETC,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_write=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_WRITE,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_tell=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_TELL,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_seek=
+{
+	23,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_POP_VAR,1,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,1,0,0,0,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_SEEK,
+		FAKE_END_PROC
+	}
+};
+
+byteCode P_rewind=
+{
+	13,
+	(char[])
+	{
+		FAKE_POP_VAR,0,0,0,0,
+		FAKE_RES_BP,
+		FAKE_PUSH_VAR,0,0,0,0,
+		FAKE_REWIND,
+		FAKE_END_PROC
+	}
+};
 
 int main(int argc,char** argv)
 {

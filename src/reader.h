ffaf0f37a5da5af0357ead56dbce6257c83460ae
ThePeiLin <pei_hang@qq.com>
2020-12-02 17:42:34 +0800

    reader macro 1

--- src/reader.h
+++ src/reader.h
@@ -1,15 +1,25 @@
 #ifndef READER_H
 #define READER_H
+#include<stdint.h>
+typedef struct String_Match_Pattern
+{
+	int32_t size;
+	char** parts;
+}StringMatchPattern;
+
 typedef struct Reader_Macro
 {
-	char* macroChr;
+	StringMatchPattern* pattern;
 	AST_cptr* expression;
 	struct Reader_Macro* next;
 }ReaderMacro;
 
-ReaderMacro* newReaderMacro(const char*,AST_cptr*);
+StringMatchPattern* newStringPattern(const char**);
+void freeStringPattern(StringMatchPattern*);
+char** splitPattern(const char*);
+ReaderMacro* newReaderMacro(StringMatchPattern*,AST_cptr*);
 void freeReaderMacro(ReaderMacro*);
-char* getMacroChr(const char*);
-ReaderMacro* findReaderMacro(const char*);
-AST_cptr* matchReaderMacro(const char*,ReaderMacro*);
+char** getMacroChr(const char*);
+ReaderMacro* stringMatch(const char*);
+AST_cptr* ReaderMacroExpand(const char*,ReaderMacro*);
 #endif

f09bd7f400cdbdb11b3690d4cbf9d2c70c15672f
ThePeiLin <pei_hang@qq.com>
2020-12-02 17:52:56 +0800

    reader macro 2

---
+++ src/reader.c
@@ -0,0 +1,28 @@
+#include"reader.h"
+#include"tool.h"
+#include<string.h>
+#include<stdlib.h>
+
+StringMatchPattern* newStringPattern(const char** parts,int32_t size)
+{
+	StringMatchPattern* tmp=(StringMatchPattern*)malloc(sizeof(StringMatchPattern));
+	if(!tmp)errors(OUTOFMEMORY,__FILE__,__LINE__);
+	tmp->size=size;
+	char** tmParts=(char**)malloc(sizeof(char*)*size);
+	if(!tmParts)errors(OUTOFMEMORY,__FILE__,__LINE__);
+	int i=0;
+	for(;i<size;i++)
+		tmParts[i]=copyStr(parts[i]);
+	tmp->parts=tmParts;
+	return tmp;
+}
+
+void freeStringPattern(StringMatchPattern* o)
+{
+	int i=0;
+	int32_t size=o->size;
+	for(;i<size;i++)
+		free(o->parts[i]);
+	free(o->parts);
+	free(o);
+}

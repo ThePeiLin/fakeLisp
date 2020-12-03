de021b19813fcdf20d232f09a5b17130987aef50
ThePeiLin <pei_hang@qq.com>
2020-12-03 08:51:35 +0800

    reader macro 3

--- src/reader.c
+++ src/reader.c
@@ -26,3 +26,20 @@ void freeStringPattern(StringMatchPattern* o)
 	free(o->parts);
 	free(o);
 }
+
+ReaderMacro* newReaderMacro(StringMatchPattern* pattern,AST_cptr* expression)
+{
+	ReaderMacro* tmp=(ReaderMacro*)malloc(sizeof(ReaderMacro));
+	if(!tmp)errors(OUTOFMEMORY,__FILE__,__LINE__);
+	tmp->pattern=StringMatchPattern;
+	tmp->expression=expression;
+	tmp->next=NULL;
+	return tmp;
+}
+
+void freeReaderMacro(ReaderMacro* tmp)
+{
+	freeStringPattern(tmp->pattern);
+	deleteCptr(tmp->expression);
+	free(tmp);
+}

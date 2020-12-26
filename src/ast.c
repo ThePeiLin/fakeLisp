fcd6c575e632fe9c39aa6beb85d153201270a06f
ThePeiLin <pei_hang@qq.com>
2020-12-26 15:12:03 +0800

    move some function to correct modules

--- src/ast.c
+++ src/ast.c
@@ -240,3 +240,13 @@ AST_cptr* castVMvalueToCptr(VMvalue* value,int32_t curline,AST_pair* prev)
 	}
 	return tmp;
 }
+
+void addToList(AST_cptr* fir,const const AST_cptr* sec)
+{
+	while(fir->type!=NIL)fir=&((AST_pair*)fir->value)->cdr;
+	fir->type=PAIR;
+	fir->value=newPair(sec->curline,fir->outer);
+	replace(&((AST_pair*)fir->value)->car,sec);
+}
+
+
--- src/ast.h
+++ src/ast.h
@@ -4,4 +4,5 @@
 #include<stdint.h>
 AST_cptr* castVMvalueToCptr(VMvalue*,int32_t,AST_pair*);
 AST_cptr* createTree(const char*,intpr*);
+static void addToList(AST_cptr*,const AST_cptr* src);
 #endif

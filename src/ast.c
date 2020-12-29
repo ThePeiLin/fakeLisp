e3317acff3b5d083dac25bab82e80b0c7bd085e9
ThePeiLin <pei_hang@qq.com>
2020-12-29 09:08:28 +0800

    fix bugs

--- src/ast.c
+++ src/ast.c
@@ -49,9 +49,14 @@ AST_cptr* createTree(const char* objStr,intpr* inter,StringMatchPattern* pattern
 						AST_cptr* tmpCptr2=createTree(parts[j]+i,inter,tmpPattern);
 						if(!tmpCptr2)
 						{
-							deleteCptr(tmpCptr);
-							free(tmpCptr);
-							return NULL;
+							if(tmpPattern)
+							{
+								deleteCptr(tmpCptr);
+								free(tmpCptr);
+								return NULL;
+							}
+							else
+								break;
 						}
 						addToList(tmpCptr,tmpCptr2);
 						deleteCptr(tmpCptr2);

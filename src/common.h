f2b4e7b3e9e1d342d1951adcd2c319865653517a
ThePeiLin <pei_hamg@qq.com>
2021-02-17 14:53:24 +0800

    delete some useless code

--- src/common.h
+++ src/common.h
@@ -105,4 +105,12 @@ void freeSymTabNode(SymTabNode*);
 void freeSymbolTable(SymbolTable*);
 
 void writeSymbolTable(SymbolTable*,FILE*);
+
+LineNumberTable* newLineNumTable();
+LineNumTabNode* newLineNumNode(int32_t id,int32_t fid,int32_t scp,int32_t cpc,int32_t line);
+LineNumTabNode* addLineNumNode(LineNumTabNode*,LineNumberTable*);
+LineNumTabNode* findLineNumTabNode(int32_t id,int32_t cp,LineNumberTable*);
+void LineNumTabNodeAppd(LineNumTabNode*,LineNumTabNode*);
+void freeLineNumTabNode(LineNumTabNode*);
+void freeLineNumberTable(LineNumberTable*);
 #endif

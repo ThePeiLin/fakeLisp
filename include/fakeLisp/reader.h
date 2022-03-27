2a2e0622c6cc9755e8cd0cc9dbd6249b4e5d3be1
ThePeiLin <pei_hang@qq.com>
2022-03-27 19:17:12 +0800

    reader.c

--- include/fakeLisp/reader.h
+++ include/fakeLisp/reader.h
@@ -3,52 +3,13 @@
 #include"ast.h"
 #include"bytecode.h"
 #include"vm.h"
+#include"pattern.h"
 #include<stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef struct FklStringMatchPattern
-{
-	uint32_t num;
-	uint32_t reserveCharNum;
-	char** parts;
-	FklValueType type;
-	union{
-		void (*fProc)(FklVM*);
-		FklByteCodelnt* bProc;
-	}u;
-	struct FklStringMatchPattern* prev;
-	struct FklStringMatchPattern* next;
-}FklStringMatchPattern;
-
-
-FklStringMatchPattern* fklFindStringPattern(const char*);
-FklStringMatchPattern* fklNewStringMatchPattern(int32_t,char**,FklByteCodelnt*);
-FklStringMatchPattern* fklNewFStringMatchPattern(int32_t num,char** parts,void(*fproc)(FklVM* exe));
-char* fklGetNthReverseCharOfStringMatchPattern(FklStringMatchPattern* pattern,uint32_t nth);
-char** fklSplitPattern(const char*,int32_t*);
-char** fklSplitStringInPattern(const char*,FklStringMatchPattern*,int32_t*);
 char* fklReadInPattern(FILE*,char**,int*);
-char* fklGetVarName(const char*);
-void fklPrintInPattern(char**,FklStringMatchPattern*,FILE*,int32_t);
-void fklFreeStringArry(char** ss,int32_t num);
-void fklFreeAllStringPattern();
-void fklFreeStringPattern(FklStringMatchPattern*);
-int fklIsInValidStringPattern(const char*);
-int fklIsReDefStringPattern(const char*);
-int fklIsVar(const char*);
-int fklIsMustList(const char*);
-size_t fklSkipInPattern(const char*,FklStringMatchPattern*);
-size_t fklSkipSpace(const char*);
-int32_t fklCountInPattern(const char* str,FklStringMatchPattern*);
-size_t fklSkipUntilNext(const char* str,const char*);
-size_t fklSkipParentheses(const char*);
-size_t fklSkipAtom(const char*,const char*);
-int32_t fklFindKeyString(const char*);
-void fklInitBuiltInStringPattern(void);
-
 #ifdef __cplusplus
 }
 #endif

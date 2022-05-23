c934aecedaf595e822e08fd7908b9863a90a23dd
ThePeiLin <pei_hang@qq.com>
2022-05-23 22:51:29 +0800

    extern "C"

--- include/fakeLisp/fklni.h
+++ include/fakeLisp/fklni.h
@@ -2,6 +2,10 @@
 #define FKL_NI_H
 #include<fakeLisp/vm.h>
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 FklVMvalue* fklNiGetArg(uint32_t* ap,FklVMstack*);
 FklVMvalue* fklNiNewVMvalue(FklValueType type,void* p,FklVMstack*,FklVMheap* heap);
 FklVMvalue* fklNiPopTop(uint32_t* ap,FklVMstack*);
@@ -11,4 +15,8 @@ int fklNiResBp(uint32_t* ap,FklVMstack*);
 void fklNiEnd(uint32_t* ap,FklVMstack*);
 void fklNiBegin(uint32_t* ap,FklVMstack*);
 #define FKL_NI_BEGIN(EXE) FklVMstack* stack=(EXE)->stack;uint32_t ap=0;fklNiBegin(&ap,stack)
+
+#ifdef __cplusplus
+}
+#endif
 #endif

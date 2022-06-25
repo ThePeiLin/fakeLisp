9a20c211f308eb60d6ff015f9e45069e3a399a9a
ThePeiLin <pei_hang@qq.com>
2022-06-26 00:17:14 +0800

    fix:symbol

--- src/parser.c
+++ src/parser.c
@@ -557,7 +557,7 @@ int fklSplitStringPartsIntoToken(char** parts,size_t* sizes,uint32_t inum,uint32
 					if(!topState||topState->pattern!=INCOMPLETE_SYMBOL)
 					{
 						fklPushPtrStack(fklNewToken(FKL_TOKEN_SYMBOL,fklNewString(size,s),*line),retvalStack);
-						if(j<sizes[i]&&!isspace(parts[i][j]))
+						if(j<sizes[i]&&(parts[i][j]=='|'||(!searchReverseStringChar(parts[i],j,sizes[i],matchStateStack)&&!isBuiltInReserveStr(parts[i]+j,sizes[i]-j))))
 						{
 							fklPushPtrStack(newMatchState(INCOMPLETE_SYMBOL,0),matchStateStack);
 							continue;
@@ -569,7 +569,8 @@ int fklSplitStringPartsIntoToken(char** parts,size_t* sizes,uint32_t inum,uint32
 						fklStringCharBufCat(&topToken->value,s,size);
 						fklPopPtrStack(matchStateStack);
 						if(!(topState&&j<sizes[i]&&(isspace(parts[i][j])
-										||(searchReverseStringChar(parts[i],j,sizes[i],matchStateStack)
+										||((isBuiltInReserveStr(parts[i]+j,sizes[i]-j)
+												||searchReverseStringChar(parts[i],j,sizes[i],matchStateStack))
 											&&parts[i][j]!='|'))))
 						{
 							fklPushPtrStack(topState,matchStateStack);

d7131d79b8d916eccbd488fb81be0ac93d092e65
ThePeiLin <pei_hang@qq.com>
2023-12-10 14:51:04 +0800

    save: hash-table -> ht

---
+++ main/ht.c
@@ -0,0 +1,326 @@
+#include<fakeLisp/vm.h>
+
+#define DECL_AND_CHECK_ARG(a,Pname) \
+	FklVMvalue* a=FKL_VM_POP_ARG(exe);\
+	if(!a)\
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_TOOFEWARG,exe);
+
+#define DECL_AND_CHECK_ARG2(a,b,Pname) \
+	FklVMvalue* a=FKL_VM_POP_ARG(exe);\
+	FklVMvalue* b=FKL_VM_POP_ARG(exe);\
+	if(!b||!a)\
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_TOOFEWARG,exe);
+
+static int ht_hashv(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.hashv";
+	DECL_AND_CHECK_ARG(key,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	uint64_t hashv=fklVMvalueEqHashv(key);
+	FKL_VM_PUSH_VALUE(exe,fklMakeVMuint(hashv,exe));
+	return 0;
+}
+
+static int ht_eqv_hashv(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.hashv";
+	DECL_AND_CHECK_ARG(key,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	uint64_t hashv=fklVMvalueEqvHashv(key);
+	FKL_VM_PUSH_VALUE(exe,fklMakeVMuint(hashv,exe));
+	return 0;
+}
+
+static int ht_equal_hashv(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.equal-hashv";
+	DECL_AND_CHECK_ARG(key,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	uint64_t hashv=fklVMvalueEqualHashv(key);
+	FKL_VM_PUSH_VALUE(exe,fklMakeVMuint(hashv,exe));
+	return 0;
+}
+
+typedef struct
+{
+	FklVMvalue* hash_func;
+	FklVMvalue* eq_func;
+	FklHashTable ht;
+}HashTable;
+
+static void htud_setVal(void* d0,const void* d1)
+{
+	*(FklVMhashTableItem*)d0=*(const FklVMhashTableItem*)d1;
+}
+
+static const FklHashTableMetaTable HtUdHashMetaTable=
+{
+	.size=sizeof(FklVMhashTableItem),
+	.__setKey=fklPtrKeySet,
+	.__setVal=htud_setVal,
+	.__uninitItem=fklDoNothingUninitHashItem,
+	.__getKey=fklHashDefaultGetKey,
+};
+
+static void ht_atomic(const FklVMudata* ud,FklVMgc* gc)
+{
+	FKL_DECL_UD_DATA(ht,HashTable,ud);
+	fklVMgcToGray(ht->hash_func,gc);
+	fklVMgcToGray(ht->eq_func,gc);
+	FklHashTable* t=&ht->ht;
+	for(FklHashTableItem* list=t->first;list;list=list->next)
+	{
+		FklVMhashTableItem* item=(FklVMhashTableItem*)list->data;
+		fklVMgcToGray(item->key,gc);
+		fklVMgcToGray(item->v,gc);
+	}
+}
+
+FKL_VM_USER_DATA_DEFAULT_PRINT(ht_print,ht);
+
+static int ht_equal(const FklVMudata* a,const FklVMudata* b)
+{
+	FKL_DECL_UD_DATA(hta,HashTable,a);
+	FKL_DECL_UD_DATA(htb,HashTable,b);
+	if(fklVMvalueEqual(hta->hash_func,htb->hash_func)
+			&&fklVMvalueEqual(htb->eq_func,htb->eq_func)
+			&&hta->ht.num==htb->ht.num)
+	{
+		FklHashTableItem* htal=hta->ht.first;
+		FklHashTableItem* htbl=htb->ht.first;
+		while(htal)
+		{
+			FklVMhashTableItem* ia=(FklVMhashTableItem*)htal->data;
+			FklVMhashTableItem* ib=(FklVMhashTableItem*)htbl->data;
+			if(fklVMvalueEqual(ia->key,ib->key)
+					&&fklVMvalueEqual(ia->v,ib->v))
+			{
+				htal=htal->next;
+				htbl=htbl->next;
+			}
+			else
+				return 0;
+		}
+		return 1;
+	}
+	else
+		return 0;
+}
+
+static void ht_finalizer(FklVMudata* ud)
+{
+	FKL_DECL_UD_DATA(ht,HashTable,ud);
+	fklUninitHashTable(&ht->ht);
+}
+
+static FklVMudMetaTable HtUdMetaTable=
+{
+	.size=sizeof(HashTable),
+	.__atomic=ht_atomic,
+	.__equal=ht_equal,
+	.__prin1=ht_print,
+	.__princ=ht_print,
+	.__finalizer=ht_finalizer,
+};
+
+static int ht_make_ht(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.make-ht";
+	DECL_AND_CHECK_ARG2(hashv,equal,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	FKL_CHECK_TYPE(hashv,fklIsCallable,Pname,exe);
+	FKL_CHECK_TYPE(equal,fklIsCallable,Pname,exe);
+	FklVMvalue* ud=fklCreateVMvalueUdata(exe,&HtUdMetaTable,ctx->proc);
+	FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(ud));
+	ht->hash_func=hashv;
+	ht->eq_func=equal;
+	fklInitHashTable(&ht->ht,&HtUdHashMetaTable);
+	FKL_VM_PUSH_VALUE(exe,ud);
+	return 0;
+}
+
+static int ht_ht_p(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht?";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+		FKL_VM_PUSH_VALUE(exe,FKL_VM_TRUE);
+	else
+		FKL_VM_PUSH_VALUE(exe,FKL_VM_NIL);
+	return 0;
+}
+
+static int ht_ht_hashv(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht-hashv";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		FKL_VM_PUSH_VALUE(exe,ht->hash_func);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+static int ht_ht_equal(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht-equal";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		FKL_VM_PUSH_VALUE(exe,ht->eq_func);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+static int ht_ht_clear(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht-clear!";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		fklClearHashTable(&ht->ht);
+		FKL_VM_PUSH_VALUE(exe,val);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+static int ht_ht_to_list(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht->list";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		FklHashTable* hash=&ht->ht;
+		FklVMvalue* r=FKL_VM_NIL;
+		FklVMvalue** cur=&r;
+		for(FklHashTableItem* list=hash->first;list;list=list->next)
+		{
+			FklVMhashTableItem* item=(FklVMhashTableItem*)list->data;
+			FklVMvalue* pair=fklCreateVMvaluePair(exe,item->key,item->v);
+			*cur=fklCreateVMvaluePairWithCar(exe,pair);
+			cur=&FKL_VM_CDR(*cur);
+		}
+		FKL_VM_PUSH_VALUE(exe,r);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+static int ht_ht_keys(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht-keys";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		FklHashTable* hash=&ht->ht;
+		FklVMvalue* r=FKL_VM_NIL;
+		FklVMvalue** cur=&r;
+		for(FklHashTableItem* list=hash->first;list;list=list->next)
+		{
+			FklVMhashTableItem* item=(FklVMhashTableItem*)list->data;
+			*cur=fklCreateVMvaluePairWithCar(exe,item->key);
+			cur=&FKL_VM_CDR(*cur);
+		}
+		FKL_VM_PUSH_VALUE(exe,r);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+static int ht_ht_values(FKL_CPROC_ARGL)
+{
+	static const char Pname[]="ht.ht-values";
+	DECL_AND_CHECK_ARG(val,Pname);
+	FKL_CHECK_REST_ARG(exe,Pname);
+	if(FKL_IS_USERDATA(val)&&FKL_VM_UD(val)->t==&HtUdMetaTable)
+	{
+		FKL_DECL_UD_DATA(ht,HashTable,FKL_VM_UD(val));
+		FklHashTable* hash=&ht->ht;
+		FklVMvalue* r=FKL_VM_NIL;
+		FklVMvalue** cur=&r;
+		for(FklHashTableItem* list=hash->first;list;list=list->next)
+		{
+			FklVMhashTableItem* item=(FklVMhashTableItem*)list->data;
+			*cur=fklCreateVMvaluePairWithCar(exe,item->v);
+			cur=&FKL_VM_CDR(*cur);
+		}
+		FKL_VM_PUSH_VALUE(exe,r);
+	}
+	else
+		FKL_RAISE_BUILTIN_ERROR_CSTR(Pname,FKL_ERR_INCORRECT_TYPE_VALUE,exe);
+	return 0;
+}
+
+struct SymFunc
+{
+	const char* sym;
+	FklVMcFunc f;
+}exports_and_func[]=
+{
+	{"hashv",       ht_hashv,       },
+	{"eqv-hashv",   ht_eqv_hashv,   },
+	{"equal-hashv", ht_equal_hashv, },
+
+	{"make-ht",     ht_make_ht,     },
+	{"ht-hashv",    ht_ht_hashv,    },
+	{"ht-equal",    ht_ht_equal,    },
+	{"ht?",         ht_ht_p,        },
+	// {"ht-ref",      ht_ht_ref,      },
+	// {"ht-ref&",     ht_ht_ref7,     },
+	// {"ht-ref$",     ht_ht_ref4,     },
+	// {"ht-ref!",     ht_ht_ref1,     },
+	// {"ht-set!",     ht_ht_set1,     },
+	// {"ht-set*!",    ht_ht_set8,     },
+	// {"ht-del!",     ht_ht_del1,     },
+	{"ht-clear!",   ht_ht_clear,   },
+	{"ht->list",    ht_ht_to_list,  },
+	{"ht-keys",     ht_ht_keys,     },
+	{"ht-values",   ht_ht_values,   },
+};
+
+static const size_t EXPORT_NUM=sizeof(exports_and_func)/sizeof(struct SymFunc);
+
+FKL_DLL_EXPORT void _fklExportSymbolInit(FKL_CODEGEN_DLL_LIB_INIT_EXPORT_FUNC_ARGS)
+{
+	*num=EXPORT_NUM;
+	FklSid_t* symbols=(FklSid_t*)malloc(sizeof(FklSid_t)*EXPORT_NUM);
+	FKL_ASSERT(symbols);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+		symbols[i]=fklAddSymbolCstr(exports_and_func[i].sym,st)->id;
+	*exports=symbols;
+}
+
+FKL_DLL_EXPORT FklVMvalue** _fklImportInit(FKL_IMPORT_DLL_INIT_FUNC_ARGS)
+{
+	FklSymbolTable* table=exe->symbolTable;
+	*count=EXPORT_NUM;
+	FklVMvalue** loc=(FklVMvalue**)malloc(sizeof(FklVMvalue*)*EXPORT_NUM);
+	FKL_ASSERT(loc);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+	{
+		FklSid_t id=fklAddSymbolCstr(exports_and_func[i].sym,table)->id;
+		FklVMcFunc func=exports_and_func[i].f;
+		FklVMvalue* dlproc=fklCreateVMvalueCproc(exe,func,dll,NULL,id);
+		loc[i]=dlproc;
+	}
+	return loc;
+}

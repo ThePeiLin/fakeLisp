943048cb1317798dcf423a5a4f0b7841f620125f
ThePeiLin <pei_hang@qq.com>
2023-12-20 22:12:52 +0800

    feat(fdb): debug bin module

---
+++ main/debug.c
@@ -0,0 +1,37 @@
+#include<fakeLisp/vm.h>
+
+struct SymFunc
+{
+	const char* sym;
+	FklVMcFunc f;
+}exports_and_func[]=
+{
+};
+
+static const size_t EXPORT_NUM=sizeof(exports_and_func)/sizeof(struct SymFunc);
+
+FKL_DLL_EXPORT void _fklExportSymbolInit(FKL_CODEGEN_DLL_LIB_INIT_EXPORT_FUNC_ARGS)
+{
+	*num=EXPORT_NUM;
+	FklSid_t* symbols=(FklSid_t*)malloc(sizeof(FklSid_t)*EXPORT_NUM);
+	FKL_ASSERT(symbols);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+		symbols[i]=fklAddSymbolCstr(exports_and_func[i].sym,st)->id;
+	*exports=symbols;
+}
+
+FKL_DLL_EXPORT FklVMvalue** _fklImportInit(FKL_IMPORT_DLL_INIT_FUNC_ARGS)
+{
+	FklSymbolTable* table=exe->symbolTable;
+	*count=EXPORT_NUM;
+	FklVMvalue** loc=(FklVMvalue**)malloc(sizeof(FklVMvalue*)*EXPORT_NUM);
+	FKL_ASSERT(loc);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+	{
+		FklSid_t id=fklAddSymbolCstr(exports_and_func[i].sym,table)->id;
+		FklVMcFunc func=exports_and_func[i].f;
+		FklVMvalue* dlproc=fklCreateVMvalueCproc(exe,func,dll,NULL,id);
+		loc[i]=dlproc;
+	}
+	return loc;
+}

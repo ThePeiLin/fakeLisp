1f885829fd0deac6cb007e227b9836210592d2ce
ThePeiLin <pei_hang@qq.com>
2024-01-25 10:07:31 +0800

    feat: libuv binding for fakeLisp

---
+++ fuv/fuv.c
@@ -0,0 +1,54 @@
+#include"fuv.h"
+
+#define FUV_PREDICATE(condtion,err_infor) FKL_DECL_AND_CHECK_ARG(val,exe,err_infor);\
+	FKL_CHECK_REST_ARG(exe,err_infor);\
+	if(condtion)\
+	FKL_VM_PUSH_VALUE(exe,FKL_VM_TRUE);\
+	else\
+	FKL_VM_PUSH_VALUE(exe,FKL_VM_NIL);\
+	return 0;
+
+static int fuv_make_loop(FKL_CPROC_ARGL)
+{
+	abort();
+	return 0;
+}
+
+static int fuv_loop_p(FKL_CPROC_ARGL){FUV_PREDICATE(isFuvLoop(val),"fuv.loop?");}
+
+struct SymFunc
+{
+	const char* sym;
+	FklVMcFunc f;
+}exports_and_func[]=
+{
+	{"make-loop", fuv_make_loop, },
+	{"loop?",     fuv_loop_p,    },
+};
+
+static const size_t EXPORT_NUM=sizeof(exports_and_func)/sizeof(struct SymFunc);
+
+FKL_DLL_EXPORT void _fklExportSymbolInit(FKL_CODEGEN_DLL_LIB_INIT_EXPORT_FUNC_ARGS)
+{
+	*num=EXPORT_NUM;
+	FklSid_t* symbols=(FklSid_t*)malloc(sizeof(FklSid_t)*EXPORT_NUM);
+	FKL_ASSERT(symbols);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+		symbols[i]=fklAddSymbolCstr(exports_and_func[i].sym,st)->id;
+	*exports=symbols;
+}
+
+FKL_DLL_EXPORT FklVMvalue** _fklImportInit(FKL_IMPORT_DLL_INIT_FUNC_ARGS)
+{
+	*count=EXPORT_NUM;
+	FklVMvalue** loc=(FklVMvalue**)malloc(sizeof(FklVMvalue*)*EXPORT_NUM);
+	FKL_ASSERT(loc);
+	for(size_t i=0;i<EXPORT_NUM;i++)
+	{
+		FklSid_t id=fklVMaddSymbolCstr(exe->gc,exports_and_func[i].sym)->id;
+		FklVMcFunc func=exports_and_func[i].f;
+		FklVMvalue* dlproc=fklCreateVMvalueCproc(exe,func,dll,NULL,id);
+		loc[i]=dlproc;
+	}
+	return loc;
+}
---
+++ fuv/fuv.h
@@ -0,0 +1,22 @@
+#ifndef FKL_FUV_FUV_H
+#define FKL_FUV_FUV_H
+
+#include<fakeLisp/vm.h>
+#include<uv.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct
+{
+	uv_loop_t loop;
+	FklHashTable gc_values;
+}FuvLoop;
+
+int isFuvLoop(FklVMvalue* v);
+#ifdef __cplusplus
+}
+#endif
+
+#endif

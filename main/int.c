08738c1ad0b953bb361bc6cac8ad73a585d8c585
ThePeiLin <pei_hang@qq.com>
2024-02-28 20:32:16 +0800

    fix(int): error occur when in interrupt function be called

--- main/int.c
+++ main/int.c
@@ -5,13 +5,47 @@ static void interrupt_handler_mark(FklVMgc* gc,void* arg)
 	fklVMgcToGray(arg,gc);
 }
 
+struct IntFrameCtx
+{
+	jmp_buf* buf;
+};
+
+FKL_CHECK_OTHER_OBJ_CONTEXT_SIZE(struct IntFrameCtx);
+
+#define DONE (1)
+#define ERR_OCCUR (2)
+
+static int int_frame_step(void* data,struct FklVM* exe)
+{
+	struct IntFrameCtx* ctx=data;
+	longjmp(*ctx->buf,DONE);
+}
+
+static const FklVMframeContextMethodTable IntFrameCtxTable=
+{
+	.step=int_frame_step,
+};
+
+static int int_err_callback(struct FklVMframe* f,FklVMvalue* ev,struct FklVM* vm)
+{
+	struct IntFrameCtx* ctx=(struct IntFrameCtx*)f->data;
+	FKL_VM_PUSH_VALUE(vm,ev);
+	longjmp(*ctx->buf,ERR_OCCUR);
+	return 0;
+}
+
 static FklVMinterruptResult interrupt_handler(FklVM* exe
 		,FklVMvalue* value
 		,FklVMvalue** pvalue
 		,void* arg)
 {
+	jmp_buf buf;
 	FklVMvalue* proc=arg;
 	FklVMframe* origin_top_frame=exe->top_frame;
+	FklVMframe* int_frame=fklCreateOtherObjVMframe(exe,&IntFrameCtxTable,exe->top_frame);
+	((struct IntFrameCtx*)int_frame->data)->buf=&buf;
+	exe->top_frame=int_frame;
+	int_frame->errorCallBack=int_err_callback;
 	uint32_t tp=exe->tp;
 	uint32_t bp=exe->bp;
 	fklSetBp(exe);
@@ -19,21 +53,23 @@ static FklVMinterruptResult interrupt_handler(FklVM* exe
 	fklCallObj(exe,proc);
 	FklVMinterruptResult result=FKL_INT_DONE;
 	exe->state=FKL_VM_READY;
-	int r=exe->thread_run_cb(exe,origin_top_frame);
-	exe->state=FKL_VM_READY;
-	while(exe->top_frame!=origin_top_frame)
-		fklPopVMframe(exe);
-	FklVMvalue* retval=FKL_VM_GET_TOP_VALUE(exe);
-	if(r)
+	int r=setjmp(buf);
+	if(r==DONE)
 	{
-		*pvalue=retval;
-		result=FKL_INT_NEXT;
-	}
-	else
-	{
+		FklVMvalue* retval=FKL_VM_GET_TOP_VALUE(exe);
 		if(retval!=FKL_VM_NIL)
 			result=FKL_INT_NEXT;
 	}
+	else if(r==ERR_OCCUR)
+	{
+		*pvalue=FKL_VM_GET_TOP_VALUE(exe);
+		result=FKL_INT_NEXT;
+	}
+	else
+		exe->thread_run_cb(exe,origin_top_frame);
+	exe->state=FKL_VM_READY;
+	while(exe->top_frame!=origin_top_frame)
+		fklPopVMframe(exe);
 	exe->bp=bp;
 	exe->tp=tp;
 	return result;
